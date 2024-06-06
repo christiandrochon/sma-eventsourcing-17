97f24d4da7d4349e55b727121e72f778ae6e962b
christiandrochon <khrisys@protonmail.com>
2024-06-06 23:59:11 +0200

    DTO pour GarageQuery et axon

--- src/main/java/fr/cdrochon/smamonolithe/event/commonapi/dto/CreateGarageQueryRequestDTO.java
+++ src/main/java/fr/cdrochon/smamonolithe/event/commonapi/dto/CreateGarageQueryRequestDTO.java
@@ -3,14 +3,19 @@ package fr.cdrochon.smamonolithe.event.commonapi.dto;
 import fr.cdrochon.smamonolithe.event.commonapi.enums.GarageStatus;
 import lombok.*;
 
+import java.time.Instant;
+
 /**
  * permet de faire le lien entre les services de l'appli et le monde exteieur
  */
 @Data
-@NoArgsConstructor @AllArgsConstructor @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreateGarageQueryRequestDTO {
-
+    private String id;
     private String nomClient;
     private String mailResponsable;
     private GarageStatus garageStatus;
+    private Instant dateQuery;
 }
