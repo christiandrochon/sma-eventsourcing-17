84dca8dcd1da4af991e23611620058843daec8f3
christiandrochon <khrisys@protonmail.com>
2024-07-03 19:47:37 +0200

    dtos Vehicule coté Command

---
+++ src/main/java/fr/cdrochon/smamonolithe/vehicule/command/dtos/VehiculeRestPostDTO.java
@@ -0,0 +1,41 @@
+package fr.cdrochon.smamonolithe.vehicule.command.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class VehiculeRestPostDTO {
+
+    private String immatriculationVehicule;
+    private Instant dateMiseEnCirculationVehicule;
+    //    private Instant dateDeValiditeControleTechnique;
+    //    private Instant dateValiditeControleTechniqueComplementaire;
+    //    private String urlCertificatImmatriculation;
+    //    private String modeleVehicule;
+    //    private String versionVehicule;
+    //    @Embedded private MarqueVehicule marqueVehicule;
+    //    @Embedded private MotorisationVehicule motorisationVehicule;
+    //    @Embedded private TypeCarburant typeCarburant;
+    //    @Embedded private TypeBoiteVitesse typeBoiteVitesse;
+    //    @Embedded private TypeDirectionAssistee typeDirectionAssistee;
+    //    @Embedded private TypeFreinage typeFreinage;
+    //    @Embedded private TypePropulsion typePropulsion;
+    //    @Embedded private TypeSuspension typeSuspension;
+    //    @Embedded private TypeVehicule typeVehicule;
+    //    private String finitionMotorisationVehicule;
+    //    private int puissanceFiscaleVehicule;
+    //    private int puissanceVehicule;
+    //    private int nombrePortesVehicule;
+    //    private int nombrePlacesVehicule;
+    //    private int kilometrageVehicule;
+    //    private int anneeVehicule;
+    //    private String couleurVehicule;
+    //    private String urlPhotoVehicule;
+    //    private boolean climatisationVehicule;
+}
