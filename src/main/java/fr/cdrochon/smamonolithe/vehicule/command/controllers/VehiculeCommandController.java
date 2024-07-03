c0004b039090a4005582c5ea11c58495ac08a150
christiandrochon <khrisys@protonmail.com>
2024-07-03 21:05:27 +0200

    controller vehicule coté command

---
+++ src/main/java/fr/cdrochon/smamonolithe/vehicule/command/controllers/VehiculeCommandController.java
@@ -0,0 +1,97 @@
+package fr.cdrochon.smamonolithe.vehicule.command.controllers;
+
+import fr.cdrochon.smamonolithe.vehicule.command.dtos.VehiculeRestPostDTO;
+import fr.cdrochon.smamonolithe.vehicule.command.services.VehiculeCommandService;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/commands")
+public class VehiculeCommandController {
+    
+    private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
+    private final EventStore eventStore;
+    private final VehiculeCommandService vehiculeCommandService;
+    
+    public VehiculeCommandController(CommandGateway commandGateway, QueryGateway queryGateway, EventStore eventStore,
+                                     VehiculeCommandService vehiculeCommandService) {
+        this.commandGateway = commandGateway;
+        this.queryGateway = queryGateway;
+        this.eventStore = eventStore;
+        this.vehiculeCommandService = vehiculeCommandService;
+    }
+    
+    /**
+     * Recoit les informations du dto, et renvoi un une commande avec les attributs du dto
+     * <p>
+     * Les attributs de la command doivcent correspondre au dto. Le controller ne fait que retourner le dto et c'est le command handler qui va se charger
+     * d'executer cette commande
+     * <p>
+     * L'id ne peut pas etre negatif
+     *
+     * @param clientRestPostDTO
+     * @return CompletableFuture<String>
+     */
+    @PostMapping(value = "/createVehicule", consumes = MediaType.APPLICATION_JSON_VALUE)
+    //    @PreAuthorize("hasRole('USER')")
+    //    @PreAuthorize("hasAuthority('USER')")
+    public CompletableFuture<String> createVehicule(@RequestBody VehiculeRestPostDTO vehiculeRestPostDTO) {
+        
+        try {
+            
+            String url = "http://localhost:8091/createVehicule";
+            HttpURLConnection httpClient = (HttpURLConnection) new URL(url).openConnection();
+            int responseCode = httpClient.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+            
+            if(responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(httpClient.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                
+                while((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                System.out.println(vehiculeRestPostDTO.toString());
+                
+                //            // Ensure the response is not HTML or XML
+                //            if(response.toString().trim().startsWith("<")) {
+                //                throw new IllegalArgumentException("Expected JSON response but received HTML/XML.");
+                //            }
+                //
+                //            // Print result
+                //            String jsonResponse = response.toString();
+                //            System.out.println(jsonResponse);
+                //
+                //            // Parse JSON response to Post object
+                //            ObjectMapper objectMapper = new ObjectMapper();
+                //            ClientRestPostDTO  clientRequestDTO = objectMapper.readValue(jsonResponse, ClientRestPostDTO.class);
+                //
+                //            // Print the post object
+                //            System.out.println(clientRequestDTO);
+                
+                return vehiculeCommandService.createVehicule(vehiculeRestPostDTO);
+            } else {
+                System.out.println("GET request not worked, response code: " + responseCode);
+                return CompletableFuture.completedFuture("Error: Unexpected response code " + responseCode);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            return CompletableFuture.completedFuture("Error: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/fr/cdrochon/smamonolithe/vehicule/command/controllers/VehiculeEventController.java
@@ -0,0 +1,30 @@
+package fr.cdrochon.smamonolithe.vehicule.command.controllers;
+
+import fr.cdrochon.smamonolithe.vehicule.query.services.VehiculeEventSourcingService;
+import org.axonframework.eventsourcing.eventstore.DomainEventStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/eventSourcing")
+public class VehiculeEventController {
+    
+    private final VehiculeEventSourcingService eventSourcingService;
+    
+    public VehiculeEventController(VehiculeEventSourcingService eventSourcingService) {
+        this.eventSourcingService = eventSourcingService;
+    }
+    
+    /**
+     * Recupere un event identifié par son id
+     * @param id id du vehicule
+     * @return DomainEventStream
+     */
+    @GetMapping("/vehiculeEvents/{id}")
+    //    @PreAuthorize("hasAuthority('USER')")
+    public DomainEventStream eventsById(@PathVariable String id){
+        return eventSourcingService.eventsByVehiculeId(id);
+    }
+}
