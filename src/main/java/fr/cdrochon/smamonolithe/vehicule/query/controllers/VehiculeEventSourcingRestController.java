04c573aecafafec4c5148db5c39b6cdf98a04783
christiandrochon <khrisys@protonmail.com>
2024-07-03 20:08:21 +0200

    Controllers Vehicule coté queries

---
+++ src/main/java/fr/cdrochon/smamonolithe/vehicule/query/controllers/VehiculeEventSourcingRestController.java
@@ -0,0 +1,32 @@
+package fr.cdrochon.smamonolithe.vehicule.query.controllers;
+
+import fr.cdrochon.smamonolithe.client.query.services.ClientEventSourcingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/eventSourcing")
+public class VehiculeEventSourcingRestController {
+    
+    private final ClientEventSourcingService eventSourcingService;
+    
+    public VehiculeEventSourcingRestController(ClientEventSourcingService eventSourcingService) {
+        this.eventSourcingService = eventSourcingService;
+    }
+    
+    /**
+     * Renvoi l'ensemble des events pour un vehicule recupéré avec son id
+     *
+     * @param id id du vehicule
+     * @return Stream
+     */
+    @GetMapping("/vehiculeQueries/{id}")
+    //    @PreAuthorize("hasAuthority('USER')")
+    public Stream eventsByAccountId(@PathVariable String id) {
+        return eventSourcingService.eventsByClientId(id).asStream();
+    }
+}
--- src/main/java/fr/cdrochon/smamonolithe/vehicule/query/controllers/VehiculeRestController.java
+++ src/main/java/fr/cdrochon/smamonolithe/vehicule/query/controllers/VehiculeRestController.java
@@ -1,72 +1,96 @@
 package fr.cdrochon.smamonolithe.vehicule.query.controllers;
 
-import fr.cdrochon.smamonolithe.client.entity.AdresseClient;
-import fr.cdrochon.smamonolithe.client.entity.Client;
-import fr.cdrochon.smamonolithe.client.repository.ClientRepository;
-import fr.cdrochon.smamonolithe.vehicule.entity.Vehicule;
-import fr.cdrochon.smamonolithe.vehicule.repository.VehiculeRepository;
+import fr.cdrochon.smamonolithe.client.command.mapper.ClientMapper;
+import fr.cdrochon.smamonolithe.client.query.dtos.ClientResponseDTO;
+import fr.cdrochon.smamonolithe.client.query.dtos.GetClientDTO;
+import fr.cdrochon.smamonolithe.client.query.entities.AdresseClient;
+import fr.cdrochon.smamonolithe.client.query.entities.Client;
+import fr.cdrochon.smamonolithe.client.query.repositories.ClientRepository;
+import fr.cdrochon.smamonolithe.vehicule.command.mapper.VehiculeMapper;
+import fr.cdrochon.smamonolithe.vehicule.query.dtos.GetVehiculeDTO;
+import fr.cdrochon.smamonolithe.vehicule.query.entities.VehiculeResponseDTO;
+import fr.cdrochon.smamonolithe.vehicule.query.repositories.VehiculeRepository;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.SubscriptionQueryResult;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping(path = "/queries")
 public class VehiculeRestController {
-
+    
+    private final QueryGateway queryGateway;
     private final VehiculeRepository vehiculeRepository;
     private final ClientRepository clientRepository;
-
-    public VehiculeRestController(VehiculeRepository vehiculeRepository, ClientRepository clientRepository) {
+    
+    public VehiculeRestController(QueryGateway queryGateway, VehiculeRepository vehiculeRepository, ClientRepository clientRepository) {
+        this.queryGateway = queryGateway;
         this.vehiculeRepository = vehiculeRepository;
         this.clientRepository = clientRepository;
     }
-
-    @GetMapping("/vehicule/{id}")
-//    @PreAuthorize("hasAuthority('USER')")
+    
+    /**
+     * Renvoi les informations considérées comme utiles à la partie query lors de la recherche d'un vehicule par son id.
+     *
+     * @param id id du vehicule
+     * @return VehiculeResponseDTO
+     */
+    @GetMapping("/vehicules/{id}")
+    //    @PreAuthorize("hasAuthority('USER')")
     //    @CircuitBreaker(name = "clientService", fallbackMethod = "getDefaultClient")
-    public Vehicule getVehiculeById(@PathVariable Long id) {
-
-        Vehicule vehicule = vehiculeRepository.findById(id).get();
-        if(vehicule.getClientId() != null) {
-            Client client = clientRepository.findById(vehicule.getId()).get();
-            vehicule.setClient(client);
-        }
-        else {
-            vehicule.setClient(getDefaultClient());
-        }
-
-
-        return vehicule;
+    public VehiculeResponseDTO getVehicule(@PathVariable String id) {
+        
+        VehiculeResponseDTO vehiculeResponseDTO = vehiculeRepository.findById(id).get();
+        GetVehiculeDTO vehiculeDTO = new GetVehiculeDTO();
+        vehiculeDTO.setId(id);
+        vehiculeResponseDTO = queryGateway.query(vehiculeDTO, VehiculeResponseDTO.class).join();
+        return vehiculeResponseDTO;
     }
-
+    
+    
+    /**
+     * Renvoi tous les vehicule.
+     * On n'utilise pas l'interface Repository usuelle, mais on créé une classe destinée à ca, qui renvoi le type de DTO sous forme de multiples instances
+     *
+     * @return List<VehiculeResponseDTO> comprenant l'adresse sous forme de DTO
+     */
     @GetMapping("/vehicules")
-//    @PreAuthorize("hasAuthority('USER')")
-    public List<Vehicule> getVehicules() {
-        List<Vehicule> vehicules = vehiculeRepository.findAll();
-
-        vehicules.forEach(c -> {
-            if(c.getClientId() != null)
-                c.setClient(clientRepository.findById(c.getClientId()).get());
-            else
-                c.setClient(getDefaultClient());
-        });
-        return vehicules;
+    //    @PreAuthorize("hasAuthority('USER')")
+    public List<VehiculeResponseDTO> getAllVehicules() {
+        List<VehiculeResponseDTO> vehicules = vehiculeRepository.findAll();
+        
+        List<VehiculeResponseDTO> vehiculeResponseDTOS = vehicules.stream()
+                                                                  .map(VehiculeMapper::convertVehiculeToVehiculeDTO)
+                                                                  .collect(Collectors.toList());
+        return vehicules.stream()
+                        .map(VehiculeMapper::convertVehiculeToVehiculeDTO)
+                        .collect(Collectors.toList());
     }
-
-    Client getDefaultClient() {
-        AdresseClient adresseClient = new AdresseClient("Numero de rue non disponible", "Rue non " +
-                "disponible", "CP non disponible", "Ville non disponible");
-
-        Client client = new Client();
-        client.setId(new Random().nextLong());
-        client.setNomClient("Non disponible");
-        client.setPrenomClient("Non disponible");
-        client.setTelClient("Non disponible");
-        client.setMailClient("Non disponible");
-        client.setAdresseClient(adresseClient);
-        //System.err.println("Exception default getDefaultClient : " + exception.getMessage());
-        return client;
+    
+    /**
+     * Renvoi un flux de vEHICULEResponseDTO qui sera mis à jour en temps réel avec de nouvelles données chaque fois qu'un nouvel événement est publié.
+     *
+     * @param id id du vehicule
+     * @return Flux de VehiculeResponseDTO
+     */
+    @GetMapping(value = "/client/{id}/watch", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<VehiculeResponseDTO> watch(@PathVariable String id) {
+        
+        try(SubscriptionQueryResult<VehiculeResponseDTO, VehiculeResponseDTO> result = queryGateway.subscriptionQuery(
+                new GetVehiculeDTO(id),
+                ResponseTypes.instanceOf(VehiculeResponseDTO.class),
+                ResponseTypes.instanceOf(VehiculeResponseDTO.class)
+                                                                                                                     )) {
+            return result.initialResult().concatWith(result.updates());
+        }
     }
 }
