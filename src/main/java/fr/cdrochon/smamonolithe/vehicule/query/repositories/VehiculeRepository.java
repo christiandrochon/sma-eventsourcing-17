14970006f74c88f1fcbe1d0b029c9e248457d193
christiandrochon <khrisys@protonmail.com>
2024-07-03 20:13:04 +0200

    Entity Vehicule et repo

--- src/main/java/fr/cdrochon/smamonolithe/vehicule/query/repositories/VehiculeRepository.java
+++ src/main/java/fr/cdrochon/smamonolithe/vehicule/query/repositories/VehiculeRepository.java
@@ -1,9 +1,9 @@
 package fr.cdrochon.smamonolithe.vehicule.query.repositories;
 
-import fr.cdrochon.smamonolithe.vehicule.query.entities.VehiculeResponseDTO;
+import fr.cdrochon.smamonolithe.vehicule.query.entities.Vehicule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehiculeRepository extends JpaRepository<VehiculeResponseDTO, String> {
+public interface VehiculeRepository extends JpaRepository<Vehicule, String> {
 }
