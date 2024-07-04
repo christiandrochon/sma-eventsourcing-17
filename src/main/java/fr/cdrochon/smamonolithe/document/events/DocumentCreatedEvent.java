b06a7736e0c2a6bcd3958c5446ac94e0c48fa6be
christiandrochon <khrisys@protonmail.com>
2024-07-04 16:40:52 +0200

    changement de constructeur

--- src/main/java/fr/cdrochon/smamonolithe/document/events/DocumentCreatedEvent.java
+++ src/main/java/fr/cdrochon/smamonolithe/document/events/DocumentCreatedEvent.java
@@ -20,22 +20,23 @@ public class DocumentCreatedEvent extends DocumentBaseEvent<String> {
     
     private final String nomDocument;
     private final String titreDocument;
-    private final TypeDocument typeDocument;
     private final String emetteurDuDocument;
+    private final TypeDocument typeDocument;
     private final Instant dateCreationDocument;
     private final Instant dateModificationDocument;
     private final DocumentStatus documentStatus;
     
-    public DocumentCreatedEvent(String id, String nomDocument, String titreDocument, TypeDocument typeDocument, String emetteurDuDocument,
+    public DocumentCreatedEvent(String id, String nomDocument, String titreDocument, String emetteurDuDocument,TypeDocument typeDocument,
                                 Instant dateCreationDocument, Instant dateModificationDocument, DocumentStatus documentStatus) {
         super(id);
         this.nomDocument = nomDocument;
         this.titreDocument = titreDocument;
-        this.typeDocument = typeDocument;
         this.emetteurDuDocument = emetteurDuDocument;
+        this.typeDocument = typeDocument;
         this.dateCreationDocument = dateCreationDocument;
         this.dateModificationDocument = dateModificationDocument;
         this.documentStatus = documentStatus;
     }
     
+
 }
