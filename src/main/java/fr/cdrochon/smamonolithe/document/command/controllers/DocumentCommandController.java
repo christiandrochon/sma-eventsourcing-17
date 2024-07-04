eeda0ff462c56c93d292fedf0aea2695370ca92e
christiandrochon <khrisys@protonmail.com>
2024-07-04 16:54:06 +0200

    controllers document coté command

---
+++ src/main/java/fr/cdrochon/smamonolithe/document/command/controllers/DocumentCommandController.java
@@ -0,0 +1,124 @@
+package fr.cdrochon.smamonolithe.document.command.controllers;
+
+import fr.cdrochon.smamonolithe.client.command.dtos.ClientRestPostDTO;
+import fr.cdrochon.smamonolithe.document.command.dtos.DocumentRestDTO;
+import fr.cdrochon.smamonolithe.document.command.services.DocumentCommandService;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/commands")
+public class DocumentCommandController {
+    
+    private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
+    private final EventStore eventStore;
+    private final DocumentCommandService documentCommandService;
+    
+    public DocumentCommandController(CommandGateway commandGateway, QueryGateway queryGateway, EventStore eventStore,
+                                     DocumentCommandService documentCommandService) {
+        this.commandGateway = commandGateway;
+        this.queryGateway = queryGateway;
+        this.eventStore = eventStore;
+        this.documentCommandService = documentCommandService;
+    }
+    
+    /**
+     * Recoit les informations du dto, et renvoi un une commande avec les attributs du dto
+     * <p>
+     * Les attributs de la command doivcent correspondre au dto. Le controller ne fait que retourner le dto et c'est le command handler qui va se charger
+     * d'executer cette commande
+     * <p>
+     * L'id ne peut pas etre negatif
+     *
+     * @param documentDTO DTO contenant les informations du document a creer
+     * @return CompletableFuture<String>
+     */
+    @PostMapping(value = "/createDocument", consumes = MediaType.APPLICATION_JSON_VALUE)
+    //    @PreAuthorize("hasRole('USER')")
+    //    @PreAuthorize("hasAuthority('USER')")
+    public CompletableFuture<String> createClient(@RequestBody DocumentRestDTO documentDTO) {
+        
+        try {
+            
+            String url = "http://localhost:8091/createDocument";
+            HttpURLConnection httpClient = (HttpURLConnection) new URL(url).openConnection();
+            int responseCode = httpClient.getResponseCode();
+            System.out.println("GET Response Code :: " + responseCode);
+            
+            if(responseCode == HttpURLConnection.HTTP_OK) { // success
+                BufferedReader in = new BufferedReader(new InputStreamReader(httpClient.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                
+                while((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                System.out.println(documentDTO.toString());
+                
+                //            // Ensure the response is not HTML or XML
+                //            if(response.toString().trim().startsWith("<")) {
+                //                throw new IllegalArgumentException("Expected JSON response but received HTML/XML.");
+                //            }
+                //
+                //            // Print result
+                //            String jsonResponse = response.toString();
+                //            System.out.println(jsonResponse);
+                //
+                //            // Parse JSON response to Post object
+                //            ObjectMapper objectMapper = new ObjectMapper();
+                //            ClientRestPostDTO  clientRequestDTO = objectMapper.readValue(jsonResponse, ClientRestPostDTO.class);
+                //
+                //            // Print the post object
+                //            System.out.println(clientRequestDTO);
+                
+                return documentCommandService.createDocument(documentDTO);
+            }
+            else {
+                System.out.println("GET request not worked, response code: " + responseCode);
+                return CompletableFuture.completedFuture("Error: Unexpected response code " + responseCode);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+            return CompletableFuture.completedFuture("Error: " + e.getMessage());
+        }
+    }
+    
+    /**
+     * Tester les events du store. On utilise l'id de l'agregat pour consulter l'etat de l'eventstore (json avec tous les events enregistrés)
+     * Le format renvoyé est du json dans swagger
+     *
+     * @param id id de l'agregat
+     * @return Stream
+     */
+    @GetMapping(path = "/eventStoreDocument/{id}")
+    //    @PreAuthorize("hasAuthority('USER')")
+    public Stream readDocumentsInEventStore(@PathVariable String id) {
+        return eventStore.readEvents(id).asStream();
+    }
+    
+    
+    /**
+     * Pour recuperer les messages d'erreur lorsqu'une requete s'est mal passée
+     *
+     * @param exception exception
+     * @return message d'erreur
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/fr/cdrochon/smamonolithe/document/command/controllers/DocumentEventController.java
@@ -0,0 +1,30 @@
+package fr.cdrochon.smamonolithe.document.command.controllers;
+
+import fr.cdrochon.smamonolithe.document.query.services.DocumentEventSourcingService;
+import org.axonframework.eventsourcing.eventstore.DomainEventStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/eventSourcing")
+public class DocumentEventController {
+    
+    private final DocumentEventSourcingService eventSourcingService;
+    
+    public DocumentEventController(DocumentEventSourcingService eventSourcingService) {
+        this.eventSourcingService = eventSourcingService;
+    }
+    
+    /**
+     * Recupere un event identifié par son id
+     * @param id id du document
+     * @return DomainEventStream
+     */
+    @GetMapping("/documentEvents/{id}")
+    //    @PreAuthorize("hasAuthority('USER')")
+    public DomainEventStream eventsById(@PathVariable String id){
+        return eventSourcingService.eventsByDocumentId(id);
+    }
+}
