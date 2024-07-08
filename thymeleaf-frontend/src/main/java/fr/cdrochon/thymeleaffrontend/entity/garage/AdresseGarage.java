c83a0ffd8c31a391eb4f519f6106f19e4599b630
christiandrochon <khrisys@protonmail.com>
2024-07-08 22:41:16 +0200

    je ne passe plus que des dtos au serveur depuis l'ihm, concernant les garages. Je n'ai fait que commenter les entités car le travail avait dejà été fait

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/entity/garage/AdresseGarage.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/entity/garage/AdresseGarage.java
@@ -1,19 +1,19 @@
-package fr.cdrochon.thymeleaffrontend.entity.garage;
-
-import jakarta.persistence.Embeddable;
-import lombok.*;
-
-@Embeddable
-@Builder
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class AdresseGarage {
-
-    private String numeroDeRue;
-    private String rue;
-    private String cp;
-    private String ville;
-}
+//package fr.cdrochon.thymeleaffrontend.entity.garage;
+//
+//import jakarta.persistence.Embeddable;
+//import lombok.*;
+//
+//@Embeddable
+//@Builder
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@ToString
+//public class AdresseGarage {
+//
+//    private String numeroDeRue;
+//    private String rue;
+//    private String cp;
+//    private String ville;
+//}
