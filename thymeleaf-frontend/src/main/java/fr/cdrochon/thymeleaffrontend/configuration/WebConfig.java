f6a184e17549d899fb4941a62d80e8f616c02829
christiandrochon <khrisys@protonmail.com>
2024-07-08 13:42:44 +0200

    config des ressources statiques

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/configuration/WebConfig.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/configuration/WebConfig.java
@@ -1,8 +1,6 @@
 package fr.cdrochon.thymeleaffrontend.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
