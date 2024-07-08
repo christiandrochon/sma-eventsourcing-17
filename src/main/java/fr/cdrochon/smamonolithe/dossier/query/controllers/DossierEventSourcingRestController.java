98bde62f409843b3425f674af4123fc28482f6fc
christiandrochon <khrisys@protonmail.com>
2024-07-09 00:57:55 +0200

    controllers coté query pour les dossiers

---
+++ src/main/java/fr/cdrochon/smamonolithe/dossier/query/controllers/DossierEventSourcingRestController.java
@@ -0,0 +1,32 @@
+package fr.cdrochon.smamonolithe.dossier.query.controllers;
+
+import fr.cdrochon.smamonolithe.dossier.query.services.DossierEventSourcingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/eventSourcing")
+public class DossierEventSourcingRestController {
+    
+    private final DossierEventSourcingService eventSourcingService;
+    
+    public DossierEventSourcingRestController(DossierEventSourcingService eventSourcingService) {
+        this.eventSourcingService = eventSourcingService;
+    }
+    
+    /**
+     * Renvoi l'ensemble des events pour un dossier recupéré avec son id
+     *
+     * @param id
+     * @return Stream
+     */
+    @GetMapping("/dossierQueries/{id}")
+    //    @PreAuthorize("hasAuthority('USER')")
+    public Stream eventsByAccountId(@PathVariable String id) {
+        return eventSourcingService.eventsByDossierId(id).asStream();
+    }
+}
