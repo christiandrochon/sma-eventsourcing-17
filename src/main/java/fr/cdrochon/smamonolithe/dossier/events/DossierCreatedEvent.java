afb046f2a45068ceea1d166f5aa3c074724a93ea
christiandrochon <khrisys@protonmail.com>
2024-07-09 01:02:14 +0200

    classe event pour les dossiers

---
+++ src/main/java/fr/cdrochon/smamonolithe/dossier/events/DossierCreatedEvent.java
@@ -0,0 +1,31 @@
+package fr.cdrochon.smamonolithe.dossier.events;
+
+import fr.cdrochon.smamonolithe.client.query.entities.Client;
+import fr.cdrochon.smamonolithe.dossier.query.entities.DossierStatus;
+import fr.cdrochon.smamonolithe.vehicule.query.entities.Vehicule;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+public class DossierCreatedEvent extends DossierBaseEvent<String> {
+    
+    
+    private final String nomDossier;
+    private final Instant dateCreationDossier;
+    private final Instant dateModificationDossier;
+    private final Client client;
+    private final Vehicule vehicule;
+    private final DossierStatus dossierStatus;
+    
+    public DossierCreatedEvent(String id, String nomDossier, Instant dateCreationDossier, Instant dateModificationDossier, Client client, Vehicule vehicule,
+                               DossierStatus dossierStatus) {
+        super(id);
+        this.nomDossier = nomDossier;
+        this.dateCreationDossier = dateCreationDossier;
+        this.dateModificationDossier = dateModificationDossier;
+        this.client = client;
+        this.vehicule = vehicule;
+        this.dossierStatus = dossierStatus;
+    }
+}
