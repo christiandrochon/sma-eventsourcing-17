9cad3af111bd4eb5a37515cf6d93cf258ff24995
christiandrochon <khrisys@protonmail.com>
2024-07-09 19:35:22 +0200

    balise h6 pour afficher les liens de retour

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/controller/dossier/CreateDossierController.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/controller/dossier/CreateDossierController.java
@@ -1,5 +1,9 @@
 package fr.cdrochon.thymeleaffrontend.controller.dossier;
 
+import fr.cdrochon.thymeleaffrontend.dtos.client.AdresseClientDTO;
+import fr.cdrochon.thymeleaffrontend.dtos.client.ClientPostDTO;
+import fr.cdrochon.thymeleaffrontend.dtos.client.ClientStatusDTO;
+import fr.cdrochon.thymeleaffrontend.dtos.client.PaysDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.document.DocumentConvertPostDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.document.DocumentPostDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.document.DocumentStatusDTO;
@@ -7,6 +11,7 @@ import fr.cdrochon.thymeleaffrontend.dtos.document.TypeDocumentDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.dossier.DossierConvertPostDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.dossier.DossierPostDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.dossier.DossierStatusDTO;
+import fr.cdrochon.thymeleaffrontend.dtos.vehicule.VehiculeStatusDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -27,20 +32,23 @@ import java.util.List;
 
 @Controller
 public class CreateDossierController {
-    
     final RestClient restClient = RestClient.create("http://localhost:8092");
     
     @GetMapping("/createDossier")
     //    @PreAuthorize("hasAuthority('ADMIN')")
     public String createDossier(Model model) {
+        ClientPostDTO clientDTO = new ClientPostDTO();
+        clientDTO.setAdresse(new AdresseClientDTO());
+        
         if(!model.containsAttribute("dossierDTO")) {
             model.addAttribute("dossierDTO", new DossierPostDTO());
         }
         
-        //chargement des listes de type de document et de status de document
-//        model.addAttribute("typeDocuments", TypeDocumentDTO.PREDEFINED_VALUES);
+        //chargement des listes de status de dossier, de status de vehicule, de status de client et de pays
+        model.addAttribute("vehiculeStatuses", List.of(VehiculeStatusDTO.values()));
         model.addAttribute("dossierStatuses", List.of(DossierStatusDTO.values()));
-        
+        model.addAttribute("clientStatuses", List.of(ClientStatusDTO.values()));
+        model.addAttribute("pays", List.of(PaysDTO.values()));
         return "dossier/createDossierForm";
     }
     
@@ -49,9 +57,13 @@ public class CreateDossierController {
     public String createDocument(@Valid @ModelAttribute("dossierDTO") DossierPostDTO dossierPostDTO, BindingResult result,
                                  RedirectAttributes redirectAttributes, Model model) {
         if(result.hasErrors()) {
-            model.addAttribute("dossierDTO", dossierPostDTO);
+            result.getAllErrors().forEach(error -> System.out.println(error.getDefaultMessage()));
             // rechargement des listes en cas d'erreur du formulaire de création
+            model.addAttribute("dossierDTO", dossierPostDTO);
             model.addAttribute("dossierStatuses", List.of(DossierStatusDTO.values()));
+            model.addAttribute("vehiculeStatuses", List.of(VehiculeStatusDTO.values()));
+            model.addAttribute("clientStatuses", List.of(ClientStatusDTO.values()));
+            model.addAttribute("pays", List.of(PaysDTO.values()));
             return "dossier/createDossierForm";
         }
         try {
@@ -66,11 +78,13 @@ public class CreateDossierController {
             LocalDate localDate = LocalDate.parse(dateCreationDossier, formatter);
             Instant dateCreation = localDate.atStartOfDay().toInstant(ZoneOffset.UTC);
             dossierConvertPostDTO.setDateCreationDossier(dateCreation);
+//            dossierConvertPostDTO.setDateCreationDossier(Instant.now());
             
             String dateModificationDossier = dossierPostDTO.getDateModificationDossier();
             LocalDate localDate1 = LocalDate.parse(dateModificationDossier, formatter);
             Instant dateModif = localDate1.atStartOfDay().toInstant(ZoneOffset.UTC);
             dossierConvertPostDTO.setDateModificationDossier(dateModif);
+//            dossierConvertPostDTO.setDateModificationDossier(Instant.now());
             
             dossierConvertPostDTO.setClient(dossierPostDTO.getClient());
             dossierConvertPostDTO.setVehicule(dossierPostDTO.getVehicule());
@@ -84,6 +98,7 @@ public class CreateDossierController {
             
             redirectAttributes.addFlashAttribute("successMessage", "Dossier created successfully");
             return "redirect:/dossiers";
+            
         } catch(Exception e) {
             System.out.println("ERREUR DANS LE FOR DOCUMENT : " + e.getMessage());
             //            return new ModelAndView("createGarageForm");
