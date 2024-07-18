d05276b17a8897971ea6c1c2402b930801c8cd6b
christiandrochon <khrisys@protonmail.com>
2024-07-19 00:24:54 +0200

    configuration de l'objectMapper de serialisation et deserialisation des objets

--- src/main/java/fr/cdrochon/smamonolithe/configuration/CustomObjectMapper.java
+++ src/main/java/fr/cdrochon/smamonolithe/configuration/CustomObjectMapper.java
@@ -1,6 +1,7 @@
 package fr.cdrochon.smamonolithe.configuration;
 
 import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -16,6 +17,8 @@ public class CustomObjectMapper {
         // Register JavaTimeModule to handle Java 8 Date/Time types
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
+        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
         
         // Augmenter la profondeur maximale
         StreamReadConstraints constraints = StreamReadConstraints.builder()
--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/configuration/WebConfig.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/configuration/WebConfig.java
@@ -1,5 +1,9 @@
 package fr.cdrochon.thymeleaffrontend.configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/templates/**")
                 .addResourceLocations("classpath:/static/");
+    }
+    
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
+        return mapper;
     }
     
 //    @Bean
