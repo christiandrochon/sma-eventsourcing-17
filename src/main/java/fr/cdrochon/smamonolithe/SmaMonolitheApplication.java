4c26b465920df12c9c25247919a3c0d86b33c73f
christiandrochon <khrisys@protonmail.com>
2024-07-19 13:12:27 +0200

    inutile de scanner les packages, le pb ne venait pas de là

--- src/main/java/fr/cdrochon/smamonolithe/SmaMonolitheApplication.java
+++ src/main/java/fr/cdrochon/smamonolithe/SmaMonolitheApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = "fr.cdrochon.smamonolithe.garage.query.mapper")
+@SpringBootApplication
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SmaMonolitheApplication {
     
