ceac8f01f41c6a8e7515630fec1223a9fbb23624
christiandrochon <khrisys@protonmail.com>
2024-07-20 00:05:12 +0200

    envoi requete coté client avec webClient
    J'utilise uniquement les DTO

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/controller/dossier/CreateDossierController.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/controller/dossier/CreateDossierController.java
@@ -1,12 +1,14 @@
 package fr.cdrochon.thymeleaffrontend.controller.dossier;
 
-import fr.cdrochon.thymeleaffrontend.dtos.client.*;
+import fr.cdrochon.thymeleaffrontend.dtos.client.ClientStatusDTO;
+import fr.cdrochon.thymeleaffrontend.dtos.client.PaysDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.dossier.DossierConvertPostDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.dossier.DossierPostDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.dossier.DossierStatusDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.vehicule.VehiculeDateConvertDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.vehicule.VehiculeStatusDTO;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.Instant;
@@ -23,11 +26,10 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import static fr.cdrochon.thymeleaffrontend.dtos.client.PaysDTO.FRANCE;
-
 @Controller
 public class CreateDossierController {
-    private final PaysListDTO paysListDTO = new PaysListDTO();
+    @Autowired
+    private WebClient webClient;
     final RestClient restClient = RestClient.create("http://localhost:8092");
     
     @GetMapping("/createDossier")
@@ -46,18 +48,9 @@ public class CreateDossierController {
         model.addAttribute("vehiculeStatuses", List.of(VehiculeStatusDTO.values()));
         model.addAttribute("dossierStatuses", List.of(DossierStatusDTO.values()));
         model.addAttribute("clientStatuses", List.of(ClientStatusDTO.values()));
-//                model.addAttribute("paysList", List.of(PaysDTO.values()));
-//                model.addAttribute("valeurParDefaut", PaysDTO.valeurParDefaut());
-                
-        model.addAttribute("paysList", paysListDTO.getPaysList());
-        model.addAttribute("paysForm", new PaysForm());
         
-        //        List.of(PaysDTO.values()).forEach(paysDTO -> {
-        //            if(paysDTO.equals(FRANCE)) {
-        //                model.addAttribute("paysDefaut", paysDTO);
-        //            }
-        //        });
-        //        model.addAttribute("paysDefaut", PaysDTO.getDefault());
+        model.addAttribute("paysList", List.of(PaysDTO.values()));
+        model.addAttribute("valeurParDefaut", PaysDTO.valeurParDefaut());
         
         return "dossier/createDossierForm";
     }
@@ -95,26 +88,62 @@ public class CreateDossierController {
             // conversion de la date de mise en circulation du vehicule
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             String dateMiseEnCirculationVehicule = dossierPostDTO.getVehicule().getDateMiseEnCirculationVehicule();
+            
             LocalDate localDate = LocalDate.parse(dateMiseEnCirculationVehicule, formatter);
             Instant date = localDate.atStartOfDay().toInstant(ZoneOffset.UTC);
             vehiculeDateConvertDTO.setDateMiseEnCirculationVehicule(date);
             
             dossierConvertPostDTO.setVehicule(vehiculeDateConvertDTO);
             
-            restClient.post().uri("/commands/createDossier")
-                      //                             .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " + getJwtTokenValue()))
-                      //                      .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                      .contentType(MediaType.APPLICATION_JSON)
-                      .body(dossierConvertPostDTO).retrieve().toBodilessEntity();
+            //            restClient.post().uri("/commands/createDossier")
+            //                      //                             .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " +
+            //                      getJwtTokenValue()))
+            //                      //                      .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            //                      .contentType(MediaType.APPLICATION_JSON)
+            //                      .body(dossierConvertPostDTO).retrieve().toBodilessEntity();
+            
+            
+            
+//            MediaType mediaType = new MediaType("application", "json", StandardCharsets.UTF_8);
+//            WebClient webClient = WebClient.builder()
+//                                           .baseUrl("http://localhost:8092")
+//                                           //                                           .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType
+//                                           //                                           .APPLICATION_JSON_VALUE)
+//                                           .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//                                           .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+//                                           .build();
+//
+//            webClient.post()
+//                                       .uri("/commands/createDossier")
+//                                       .contentType(MediaType.APPLICATION_JSON)
+//                                       .acceptCharset(StandardCharsets.UTF_8)
+//                                       .bodyValue(dossierConvertPostDTO)
+//                                       .retrieve()
+//                                       .bodyToMono(String.class)
+//                                       .subscribe(response -> {
+//                                           System.out.println("Response: " + response);
+//                                       }, error -> {
+//                                           System.err.println("Error: " + error.getMessage());
+//                                       });
+////                                       .block();
+            webClient.post()
+                     .uri("/commands/createDossier")
+                     .contentType(MediaType.APPLICATION_JSON)
+                     .bodyValue(dossierConvertPostDTO)
+                     .retrieve()
+                     .bodyToMono(String.class)
+                     .block();
+            
+            System.out.println("Dossier created successfully");
+            
             
             redirectAttributes.addFlashAttribute("successMessage", "Dossier created successfully");
             return "redirect:/dossiers";
             
         } catch(Exception e) {
-            System.out.println("ERREUR DANS LE FOR DOCUMENT : " + e.getMessage());
-            
+            System.out.println("ERREUR DANS LE DOSSIER : " + e.getMessage());
             redirectAttributes.addFlashAttribute("errorMessage", "An error occurred: " + e.getMessage());
-            redirectAttributes.addFlashAttribute("documentDTO", dossierPostDTO); // Re-add garageDTO to the model if there's an error
+            redirectAttributes.addFlashAttribute("dossierDTO", dossierPostDTO); // Re-add garageDTO to the model if there's an error
             return "redirect:/createDossier";
         }
     }
