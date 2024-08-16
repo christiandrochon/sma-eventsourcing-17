d7d7c46f2258d6275ae47d8a742d6c0a35fd678c
christiandrochon <khrisys@protonmail.com>
2024-08-16 23:42:52 +0200

    nettoyage

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/dtos/vehicule/inner/GetImmatriculationDTO.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/dtos/vehicule/inner/GetImmatriculationDTO.java
@@ -1,6 +1,5 @@
 package fr.cdrochon.thymeleaffrontend.dtos.vehicule.inner;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
@@ -11,7 +10,6 @@ import lombok.*;
 @AllArgsConstructor
 public class GetImmatriculationDTO {
     
-//    @NotBlank(message = "L'immatriculation du véhicule est obligatoire, merci de la renseigner.")
     @Pattern(regexp = "^[A-Z]{2}-\\d{3}-[A-Z]{2}$", message = "Format incorrect.")
     private String immatriculation;
 }
