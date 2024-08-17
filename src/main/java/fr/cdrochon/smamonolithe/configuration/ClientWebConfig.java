80cab0610ab518db526e16f333794cafc65ae535
christiandrochon <khrisys@protonmail.com>
2024-08-17 19:54:55 +0200

    configuration des client de requete web coté serveur

--- src/main/java/fr/cdrochon/smamonolithe/configuration/ClientWebConfig.java
+++ src/main/java/fr/cdrochon/smamonolithe/configuration/ClientWebConfig.java
@@ -1,13 +1,16 @@
 package fr.cdrochon.smamonolithe.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class RestTemplateConfiguration {
+public class ClientWebConfig {
     
+    @Value("external.service.url")
+    private String externalServiceUrl;
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
@@ -15,6 +18,6 @@ public class RestTemplateConfiguration {
     
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().baseUrl("http://localhost:8091").build();
+        return WebClient.builder().baseUrl(externalServiceUrl).build();
     }
 }
