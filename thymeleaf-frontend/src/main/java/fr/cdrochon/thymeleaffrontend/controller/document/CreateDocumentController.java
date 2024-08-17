3bc95aba11d4974eafcdc724ea15449ea9aac88a
christiandrochon <khrisys@protonmail.com>
2024-08-17 20:41:24 +0200

    requete client/serveur pour create document => ok

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/controller/document/CreateDocumentController.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/controller/document/CreateDocumentController.java
@@ -1,10 +1,13 @@
 package fr.cdrochon.thymeleaffrontend.controller.document;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import fr.cdrochon.thymeleaffrontend.dtos.document.DocumentConvertPostDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.document.DocumentPostDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.document.DocumentStatusDTO;
 import fr.cdrochon.thymeleaffrontend.dtos.document.TypeDocumentDTO;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
@@ -22,22 +26,28 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.List;
 
+@Slf4j
 @Controller
 public class CreateDocumentController {
     
-    final RestClient restClient = RestClient.create("http://localhost:8092");
+    @Value("${external.service.url}")
+    private String externalServiceUrl;
+    private final RestClient restClient;
+    
+    public CreateDocumentController(RestClient restClient) {
+        this.restClient = restClient;
+    }
     
     @GetMapping("/createDocument")
     //    @PreAuthorize("hasAuthority('ADMIN')")
     public String createDocument(Model model) {
+        
         if(!model.containsAttribute("documentDTO")) {
             model.addAttribute("documentDTO", new DocumentPostDTO());
         }
-        
         //chargement des listes de type de document et de status de document
         model.addAttribute("typeDocuments", TypeDocumentDTO.PREDEFINED_VALUES);
         model.addAttribute("documentStatuses", List.of(DocumentStatusDTO.values()));
-        
         return "document/createDocumentForm";
     }
     
@@ -75,8 +85,8 @@ public class CreateDocumentController {
             document.setDateModificationDocument(dateModif);
             
             document.setDocumentStatus(documentPostDTO.getDocumentStatus());
-            
-            restClient.post().uri("/commands/createDocument")
+            //TODO : requete async
+            restClient.post().uri(externalServiceUrl + "/commands/createDocument")
                       //                             .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " + getJwtTokenValue()))
                       //                      .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                       .contentType(MediaType.APPLICATION_JSON)
@@ -85,11 +95,9 @@ public class CreateDocumentController {
             redirectAttributes.addFlashAttribute("successMessage", "Document created successfully");
             return "redirect:/documents";
         } catch(Exception e) {
-            System.out.println("ERREUR DANS LE FOR DOCUMENT : " + e.getMessage());
-            //            return new ModelAndView("createGarageForm");
+            log.error("An error occurred: {}", e.getMessage());
             redirectAttributes.addFlashAttribute("errorMessage", "An error occurred: " + e.getMessage());
             redirectAttributes.addFlashAttribute("documentDTO", documentPostDTO); // Re-add garageDTO to the model if there's an error
-            //            return new ModelAndView("redirect:/createGarage");
             return "redirect:/createDocument";
         }
     }
