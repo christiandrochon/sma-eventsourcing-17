24b09afc0ba8407c6816ef09059495e370959c46
christiandrochon <khrisys@protonmail.com>
2024-08-22 01:42:08 +0200

    traitement des reuqtes de recherche du'un client en interne avec des requete asynchrones de maniere à garder une ihm fluide. Coté serveur, c'est pas la peine

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/controller/client/SearchClientThymController.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/controller/client/SearchClientThymController.java
@@ -1,67 +1,132 @@
 package fr.cdrochon.thymeleaffrontend.controller.client;
 
 import fr.cdrochon.thymeleaffrontend.dtos.client.ClientThymDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Controller
+@Slf4j
 public class SearchClientThymController {
-    
-    @Value("${external.service.url}")
-    private String externalServiceUrl;
     
     @Autowired
     private WebClient webClient;
-    private final RestClient restClient;
-    
-    public SearchClientThymController(RestClient restClient) {
-        this.restClient = restClient;
-    }
     
     /**
-     * Affiche l'ensemble des clients dans la liste déroulante du formulaire de recherche de client
+     * Affiche l'ensemble des clients dans la liste déroulante du formulaire de recherche de client.
+     *
+     * Preference d'utiliser l'approche asynchrone pour la fluidité de l'application
      * @param model model de la vue
-     * @return la vue searchClientForm
+     * @param redirectAttributes attributs de redirection
+     * @return la vue client/searchClientForm
      */
     @GetMapping(value = "/searchclient")
-    public String searchClient(Model model) {
-        List<ClientThymDTO> clients =
-                restClient.get()
-                          .uri(externalServiceUrl + "/queries/clients")
-                          //                          .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " + getJwtTokenValue()))
-                          .retrieve().body(new ParameterizedTypeReference<>() {
-                          });
-        model.addAttribute("clients", clients);
-        model.addAttribute("clientPostDTO", new ClientThymDTO());
+    //    @PreAuthorize("hasAuthority('USER')")
+    public Mono<String> searchClientsAsync(Model model, RedirectAttributes redirectAttributes) {
+        return webClient.get()
+                        .uri("/queries/clients")
+                        .accept(MediaType.APPLICATION_JSON)
+                        //                                         .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION,
+                        //                                         "Bearer " + getJwtTokenValue()))
+                        .retrieve()
+                        .bodyToFlux(ClientThymDTO.class)
+                        .collectList()
+                        .flatMap(clients -> {
+                            assert clients != null;
+                            model.addAttribute("clients", clients);
+                            model.addAttribute("clientPostDTO", new ClientThymDTO());
+                            return Mono.just("client/searchClientForm");
+                        })
+                        .onErrorResume(WebClientResponseException.class, e -> {
+                            if(e.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                                log.error("400 Bad Request: {}", e.getResponseBodyAsString());
+                                redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+                                redirectAttributes.addFlashAttribute("errorMessage", "Requête invalide.");
+                                redirectAttributes.addFlashAttribute("urlRedirection", "/createGarage");
+                                return Mono.just("redirect:/error");
+                            } else if(e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                                log.error("404 Not Found: {}", e.getResponseBodyAsString());
+                                redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+                                redirectAttributes.addFlashAttribute("errorMessage", "Ressource non trouvée. " + e.getResponseBodyAsString());
+                                redirectAttributes.addFlashAttribute("urlRedirection", "/clients");
+                                return Mono.just("redirect:/error");
+                            } else if(e.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
+                                log.error("500 Internal Server Error: {}", e.getResponseBodyAsString());
+                                redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+                                redirectAttributes.addFlashAttribute("errorMessage",
+                                                                     "Erreur interne de serveur. " + e.getResponseBodyAsString());
+                                redirectAttributes.addFlashAttribute("urlRedirection", "/createGarage");
+                                return Mono.just("redirect:/error");
+                            }
+                            log.error("ERREUR: {}", e.getResponseBodyAsString());
+                            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+                            redirectAttributes.addFlashAttribute("errorMessage", "Erreur non reconnue. " + e.getResponseBodyAsString());
+                            redirectAttributes.addFlashAttribute("urlRedirection", "/clients");
+                            return Mono.just("redirect:/error");
+                        });
         
-        return "client/searchClientForm";
     }
+//    @GetMapping(value = "/searchclient")
+//    public String searchClient(Model model) {
+//        List<ClientThymDTO> clients =
+//                restClient.get()
+//                          .uri(externalServiceUrl + "/queries/clients")
+//                          //                          .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " + getJwtTokenValue()))
+//                          .retrieve().body(new ParameterizedTypeReference<>() {
+//                          });
+//        model.addAttribute("clients", clients);
+//        model.addAttribute("clientPostDTO", new ClientThymDTO());
+//
+//        return "client/searchClientForm";
+//    }
     
     /**
-     * Recherche un client par son id
-     * @param id   id du client
+     * Recherche un client par son id et affiche ses informations.
+     *
+     * Preference d'utiliser l'approche asynchrone pour la fluidité de l'application
+     *
+     * @param id    id du client
      * @param model model de la vue
      * @return la vue client/view
      */
-    @GetMapping("/searchclient/{id}")
+    @GetMapping(value = "/searchclient/{id}")
     //    @PreAuthorize("hasAuthority('USER')")
-    public String searchClientById(@PathVariable String id, Model model) {
-        ClientThymDTO client = restClient.get().uri(externalServiceUrl + "/queries/clients/" + id)
-                                         //                                  .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " +
-                                         //                                  getJwtTokenValue()))
-                                         .retrieve().body(new ParameterizedTypeReference<>() {
-                });
-        model.addAttribute("client", client);
-        return "client/view";
+    public Mono<String> searchClientByIdAsync(@PathVariable String id, Model model) {
+        return webClient.get()
+                        .uri("/queries/clients/" + id)
+                        //                                  .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " +
+                        //                                  getJwtTokenValue()))
+                        .retrieve()
+                        .bodyToMono(ClientThymDTO.class)
+                        .onErrorResume(throwable -> Mono.error(new RuntimeException("Erreur lors de la récupération du client")))
+                        .flatMap(clientThymDTO -> {
+                            model.addAttribute("client", clientThymDTO);
+                            return Mono.just("client/view");
+                        });
     }
+    
+//    @GetMapping("/searchclient/{id}")
+//    //    @PreAuthorize("hasAuthority('USER')")
+//    public String searchClientById(@PathVariable String id, Model model) {
+//        ClientThymDTO client = restClient.get().uri(externalServiceUrl + "/queries/clients/" + id)
+//                                         //                                  .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " +
+//                                         //                                  getJwtTokenValue()))
+//                                         .retrieve().body(new ParameterizedTypeReference<>() {
+//                });
+//        model.addAttribute("client", client);
+//        return "client/view";
+//    }
 
 }
