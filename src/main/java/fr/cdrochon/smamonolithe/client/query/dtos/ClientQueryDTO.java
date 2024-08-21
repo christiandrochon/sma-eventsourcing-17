67eb483bb993d0a6e59f6732012a0785f3d102f1
christiandrochon <khrisys@protonmail.com>
2024-08-22 01:09:11 +0200

    enum non utile à convertir en dto

--- src/main/java/fr/cdrochon/smamonolithe/client/query/dtos/ClientQueryDTO.java
+++ src/main/java/fr/cdrochon/smamonolithe/client/query/dtos/ClientQueryDTO.java
@@ -1,5 +1,6 @@
 package fr.cdrochon.smamonolithe.client.query.dtos;
 
+import fr.cdrochon.smamonolithe.client.command.enums.ClientStatus;
 import lombok.*;
 
 @Getter
@@ -8,13 +9,13 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class ClientResponseDTO {
+public class ClientQueryDTO {
+    
     private String id;
     private String nomClient;
     private String prenomClient;
     private String mailClient;
     private String telClient;
-//    private AdresseClient adresse;
     private ClientAdresseDTO adresse;
-    private ClientStatusDTO clientStatus;
+    private ClientStatus clientStatus;
 }
