cb86d78e229056444eb3fc7b33d4d9cdeb6f45b5
christiandrochon <khrisys@protonmail.com>
2024-08-21 03:07:17 +0200

    suppression fichier devenus inutiles

--- src/main/java/fr/cdrochon/smamonolithe/garage/query/repositories/GarageTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.cdrochon.smamonolithe.garage.query.repositories;
-
-import fr.cdrochon.smamonolithe.garage.query.entities.GarageTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GarageTransactionRepository extends JpaRepository<GarageTransaction, Long> {
-}
