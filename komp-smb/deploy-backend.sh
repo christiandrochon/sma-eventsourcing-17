09a37b963c08a6f6dcc3589fbd3397738853bb43
christiandrochon <khrisys@protonmail.com>
2024-08-22 15:49:46 +0200

    script de deploiement minikube en poussant les images sur docker hub = ok fichiers yaml ok

--- komp-smb/deploy-backend.sh
+++ komp-smb/deploy-backend.sh
@@ -3,14 +3,22 @@
 # Naviguer à la racine du projet
 cd "$(dirname "$0")/.."
 
-# Construire le projet Maven et créer l'image Docker pour sma-monolithe
 mvn clean package -DskipTests
-docker build . -t sma-monolithe:v1.0
+docker build . -t sma-monolithe:v1.1
 
-# charge l'image Docker dans Minikube
-minikube image load sma-monolithe:v1.0
+docker tag sma-monolithe:v1.1 btrurqnt/sma-monolithe:v1.1
+echo $DOCKER_PASSWORD | docker login -u btrurqnt --password-stdin
+docker push btrurqnt/sma-monolithe:v1.1
 
-# Appliquer les fichiers de déploiement Kubernetes
+# Arrêter et supprimer les conteneurs utilisant l'image
+docker ps -q --filter ancestor=btrurqnt/sma-monolithe:v1.1 | xargs -r docker stop
+docker ps -a -q --filter ancestor=btrurqnt/sma-monolithe:v1.1 | xargs -r docker rm
+
+# En cas de changement du code source via la jar, suppression de l'ancienne image
+minikube ssh -- docker image rmi -f btrurqnt/sma-monolithe:v1.1
+minikube image load btrurqnt/sma-monolithe:v1.1
+
+# Déploiement
 cd komp-smb
 kubectl apply -f sma-monolithe-deployment.yaml -n smb
 
--- komp-smb/deploy-frontend.sh
+++ komp-smb/deploy-frontend.sh
@@ -10,15 +10,26 @@ PATH=$JAVA_HOME/bin:$PATH
 # Naviguer dans le dossier thymeleaf-frontend
 cd thymeleaf-frontend
 
-# Construire le projet Maven et créer l'image Docker pour sma-monolithe
+# Construire le projet Maven et créer l'image Docker pour sma-thymeleaf
 mvn clean package
-docker build . -t sma-thymeleaf:v1.0
+docker build . -t sma-thymeleaf:v1.1
 
-# Charger les images Docker dans Minikube
-minikube image load sma-thymeleaf:v1.0
+docker tag sma-thymeleaf:v1.1 btrurqnt/sma-thymeleaf:v1.1
+echo $DOCKER_PASSWORD | docker login -u btrurqnt --password-stdin
+docker push btrurqnt/sma-thymeleaf:v1.1
+
+# Arrêter tous les conteneurs utilisant l'image sma-thymeleaf
+docker ps -q --filter ancestor=btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker stop
+# Supprimer tous les conteneurs utilisant l'image sma-thymeleaf
+docker ps -a -q --filter ancestor=btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker rm
+# Supprimer toutes les images sma-thymeleaf
+docker images -q btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker rmi -f
+
+# En cas de changement du code source via la jar, suppression de l'ancienne image
+minikube ssh -- docker image rmi -f btrurqnt/sma-thymeleaf:v1.1
+minikube image load btrurqnt/sma-thymeleaf:v1.1
 
 cd ..
 # Appliquer les fichiers de déploiement Kubernetes
 cd komp-smb
-#kubectl apply -f sma-monolithe-deployment.yaml -n smb
 kubectl apply -f thymeleaf-frontend-deployment.yaml -n smb
