f1c325bf8d926daa9d9fcfe8dacdfd50edd695fe
christiandrochon <khrisys@protonmail.com>
2024-08-23 14:59:49 +0200

    scripts de deploiement sur minikube

--- komp-smb/deploy-backend.sh
+++ komp-smb/deploy-backend.sh
@@ -4,18 +4,18 @@
 cd "$(dirname "$0")/.."
 
 mvn clean package -DskipTests
-docker build . -t sma-monolithe:v1.1
-
+docker build --no-cache . -t sma-monolithe:v1.1
 docker tag sma-monolithe:v1.1 btrurqnt/sma-monolithe:v1.1
+
 echo $DOCKER_PASSWORD | docker login -u btrurqnt --password-stdin
 docker push btrurqnt/sma-monolithe:v1.1
 
 # Arrêter et supprimer les conteneurs utilisant l'image
-docker ps -q --filter ancestor=btrurqnt/sma-monolithe:v1.1 | xargs -r docker stop
-docker ps -a -q --filter ancestor=btrurqnt/sma-monolithe:v1.1 | xargs -r docker rm
-
+#docker ps -q --filter ancestor=btrurqnt/sma-monolithe:v1.1 | xargs -r docker stop
+#docker ps -a -q --filter ancestor=btrurqnt/sma-monolithe:v1.1 | xargs -r docker rm
 # En cas de changement du code source via la jar, suppression de l'ancienne image
-minikube ssh -- docker image rmi -f btrurqnt/sma-monolithe:v1.1
+#minikube ssh -- docker image rmi -f btrurqnt/sma-monolithe:v1.1
+
 minikube image load btrurqnt/sma-monolithe:v1.1
 
 # Déploiement
--- komp-smb/deploy-frontend.sh
+++ komp-smb/deploy-frontend.sh
@@ -3,7 +3,7 @@
 # Naviguer à la racine du projet
 cd "$(dirname "$0")/.."
 
-# Temporarily set JAVA_HOME for using JDK 17.0.12 for Maven commands
+# Le compilateur a besoin de la version 17 alors que mon systeme est defini sur la version 8
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 PATH=$JAVA_HOME/bin:$PATH
 
@@ -12,21 +12,21 @@ cd thymeleaf-frontend
 
 # Construire le projet Maven et créer l'image Docker pour sma-thymeleaf
 mvn clean package
-docker build . -t sma-thymeleaf:v1.1
-
+docker build --no-cache . -t sma-thymeleaf:v1.1
 docker tag sma-thymeleaf:v1.1 btrurqnt/sma-thymeleaf:v1.1
+
 echo $DOCKER_PASSWORD | docker login -u btrurqnt --password-stdin
 docker push btrurqnt/sma-thymeleaf:v1.1
 
-# Arrêter tous les conteneurs utilisant l'image sma-thymeleaf
-docker ps -q --filter ancestor=btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker stop
-# Supprimer tous les conteneurs utilisant l'image sma-thymeleaf
-docker ps -a -q --filter ancestor=btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker rm
-# Supprimer toutes les images sma-thymeleaf
-docker images -q btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker rmi -f
+## Arrêter tous les conteneurs utilisant l'image sma-thymeleaf
+#docker ps -q --filter ancestor=btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker stop
+## Supprimer tous les conteneurs utilisant l'image sma-thymeleaf
+#docker ps -a -q --filter ancestor=btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker rm
+## Supprimer toutes les images sma-thymeleaf
+#docker images -q btrurqnt/sma-thymeleaf:v1.1 | xargs -r docker rmi -f
+## En cas de changement du code source via la jar, suppression de l'ancienne image
+#minikube ssh -- docker image rmi -f btrurqnt/sma-thymeleaf:v1.1
 
-# En cas de changement du code source via la jar, suppression de l'ancienne image
-minikube ssh -- docker image rmi -f btrurqnt/sma-thymeleaf:v1.1
 minikube image load btrurqnt/sma-thymeleaf:v1.1
 
 cd ..
