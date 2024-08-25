85547890bcc1cbf724e011f57006cbedd29db6b4
christiandrochon <khrisys@protonmail.com>
2024-08-25 14:57:16 +0200

    config de webclient (je garde les tests en commentaire en cas de nouveaux pbs avec json)

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/configuration/ClientConfig.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/configuration/ClientConfig.java
@@ -22,33 +22,65 @@ public class ClientConfig {
     @Value("${external.service.url}")
     private String externalServiceUrl;
     
-        @Autowired
-        private ObjectMapper objectMapper;
-
-    
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                        .codecs(configurer -> {
-                            configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
-                            configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
-                        })
-                        .filter((request, next) -> {
-                            System.out.println("Request: " + request.url());
-                            return next.exchange(request).doOnNext(response -> {
-                                response.body((clientHttpResponse, context) -> {
-                                    clientHttpResponse.getBody().subscribe(dataBuffer -> {
-                                        byte[] bytes = new byte[dataBuffer.readableByteCount()];
-                                        dataBuffer.read(bytes);
-                                        System.out.println("Response body: " + new String(bytes, StandardCharsets.UTF_8));
-                                    });
-                                    return Mono.empty();
-                                });
-                            });
-                        })
                         .baseUrl(externalServiceUrl)
                         .build();
     }
+    
+    //        @Autowired
+    //        private ObjectMapper objectMapper;
+    //    public ObjectMapper objectMapper() {
+    //        ObjectMapper mapper = new ObjectMapper();
+    //        mapper.registerModule(new JavaTimeModule());
+    //        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+    //        mapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true);
+    //        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    //        return mapper;
+    //    }
+    
+    //    @Bean
+    //    public WebClient webClient() {
+    //        ExchangeStrategies strategies =
+    //                ExchangeStrategies.builder()
+    //                                  .codecs(clientDefaultCodecsConfigurer -> {
+    //                                      clientDefaultCodecsConfigurer.defaultCodecs()
+    //                                                                   .jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper()));
+    //                                      clientDefaultCodecsConfigurer.defaultCodecs()
+    //                                                                   .jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper()));
+    //                                  })
+    //                                  .build();
+    //
+    //        return WebClient.builder()
+    //                        .exchangeStrategies(strategies)
+    //                        .baseUrl(externalServiceUrl)
+    //                        .build();
+    //    }
+    
+    //    @Bean
+    //    public WebClient webClient() {
+    //        return WebClient.builder()
+    //                        .codecs(configurer -> {
+    //                            configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper()));
+    //                            configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper()));
+    //                        })
+    //                        //                        .filter((request, next) -> {
+    //                        //                            System.out.println("Request: " + request.url());
+    //                        //                            return next.exchange(request).doOnNext(response -> {
+    //                        //                                response.body((clientHttpResponse, context) -> {
+    //                        //                                    clientHttpResponse.getBody().subscribe(dataBuffer -> {
+    //                        //                                        byte[] bytes = new byte[dataBuffer.readableByteCount()];
+    //                        //                                        dataBuffer.read(bytes);
+    //                        //                                        System.out.println("Response body: " + new String(bytes, StandardCharsets.UTF_8));
+    //                        //                                    });
+    //                        //                                    return Mono.empty();
+    //                        //                                });
+    //                        //                            });
+    //                        //                        })
+    //                        .baseUrl(externalServiceUrl)
+    //                        .build();
+    //    }
     
     @Bean
     public RestClient restClient() {
