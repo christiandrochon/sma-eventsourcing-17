c7fb237bef648e7d615066f46f5695c16522f2f5
christiandrochon <khrisys@protonmail.com>
2024-08-25 14:53:43 +0200

    ajout contrainte de validation thymeleaf

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/dtos/client/AdresseClientDTO.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/dtos/client/AdresseClientDTO.java
@@ -3,6 +3,7 @@ package fr.cdrochon.thymeleaffrontend.dtos.client;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -14,6 +15,7 @@ public class AdresseClientDTO {
     @NotBlank(message = "Ce champ est obligatoire")
     private String numeroDeRue;
     @NotBlank(message = "Ce champ est obligatoire")
+    @Size(min = 2, message = "Le nom de la rue doit contenir au moins 2 caractères.")
     private String rue;
     private String complementAdresse;
     @Pattern(regexp = "^[0-9]{5}$", message = "Code postal invalide")
