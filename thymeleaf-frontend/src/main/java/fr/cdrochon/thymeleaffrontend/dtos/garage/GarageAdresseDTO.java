6aa87170e2b5e7abb4db51fb01b9cd4cccc88db4
christiandrochon <khrisys@protonmail.com>
2024-08-25 14:51:02 +0200

    suppression validation qui pose pb avec json

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/dtos/garage/GarageAdresseDTO.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/dtos/garage/GarageAdresseDTO.java
@@ -2,6 +2,7 @@ package fr.cdrochon.thymeleaffrontend.dtos.garage;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,12 +11,13 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class GarageAdresseDTO {
-    @NotBlank(message = "Champ obligatoire")
+//    @NotBlank(message = "Champ obligatoire")
     private String numeroDeRue;
-    @NotBlank(message = "Champ obligatoire")
+//    @NotBlank(message = "Champ obligatoire")
+    @Size(min = 2, message = "Le nom de la rue doit contenir au moins 2 caractères.")
     private String rue;
     @Pattern(regexp = "^[0-9]{5}$", message = "Code postal invalide")
     private String cp;
-    @NotBlank(message = "Champ obligatoire")
+//    @NotBlank(message = "Champ obligatoire")
     private String ville;
 }
