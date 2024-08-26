f7a342158239af15619424b633ee249b131aef1a
christiandrochon <khrisys@protonmail.com>
2024-08-26 21:51:28 +0200

    renommage des dto pour s'y retrouver plus facilement

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/dtos/vehicule/inner/VehiculeThymDTO.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/dtos/vehicule/inner/VehiculeThymDTO.java
@@ -7,7 +7,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class VehiculeDTO {
+public class VehiculeThymDTO {
     
     private String id;
     private String immatriculationVehicule;
