eed89f08deca202d9eea403e7b3d9fc4fa691e73
christiandrochon <khrisys@protonmail.com>
2024-08-27 23:20:20 +0200

    Convertit les entités client et vehicule en dto respectifs

--- src/main/java/fr/cdrochon/smamonolithe/vehicule/query/mapper/RecursiveConversionClientVehicule.java
+++ src/main/java/fr/cdrochon/smamonolithe/vehicule/query/mapper/RecursiveConversionClientVehicule.java
@@ -10,18 +10,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static fr.cdrochon.smamonolithe.client.query.mapper.AdresseQueryMapper.convertAdresseToClientAdresseDTO;
-import static fr.cdrochon.smamonolithe.client.query.mapper.ClientQueryMapper.convertClientToClientDTO;
-import static fr.cdrochon.smamonolithe.vehicule.query.mapper.VehiculeQueryMapper.convertVehiculeToVehiculeDTO;
 
 public class RecursiveConversionClientVehicule {
     
     private static final Map<String, ClientQueryDTO> clientMap = new HashMap<>();
     private static final Map<String, VehiculeQueryDTO> vehiculeMap = new HashMap<>();
     
-    public static ClientQueryDTO addClientQueryMapper(Client client) {
+    /**
+     * Convertit le contenu d'une entité client dans un ClientQueryDTO, tout en controllant les appels recursifs de la création d'un vehicule dans un dto
+     * client
+     *
+     * @param client Client à ajouter dans le DTO
+     * @return ClientQueryDTO contenant les informations du client
+     */
+    public static ClientQueryDTO convertClientToClientQueryDTO(Client client) {
+        
         if(clientMap.containsKey(client.getId())) {
             return clientMap.get(client.getId());
         }
+        
         ClientQueryDTO clientQueryDTO = new ClientQueryDTO();
         //control de l'appel recursif des methodes elles-memes grace à la map
         clientMap.put(client.getId(), clientQueryDTO);
@@ -39,13 +46,20 @@ public class RecursiveConversionClientVehicule {
         
         // control de l'appel recursif de l'objet vehicule
         if(client.getVehicule() != null && !vehiculeMap.containsKey(client.getVehicule().getId())) {
-            clientQueryDTO.setVehicule(convertVehiculeToVehiculeDTO(client.getVehicule()));
+            clientQueryDTO.setVehicule(convertVehiculeToVehiculeQueryDTO(client.getVehicule()));
         }
         
         return clientQueryDTO;
     }
     
-    public static VehiculeQueryDTO addVehiculeQueryMapper(Vehicule vehicule) {
+    /**
+     * Convertit le contenu d'une entité vehicule dans un VehiculeQueryDTO, tout en controllant les appels recursifs de la création d'un client dans ce dto
+     * vehicule
+     *
+     * @param vehicule Vehicule à ajouter dans le DTO
+     * @return VehiculeQueryDTO contenant les informations du vehicule
+     */
+    public static VehiculeQueryDTO convertVehiculeToVehiculeQueryDTO(Vehicule vehicule) {
         if(vehiculeMap.containsKey(vehicule.getId())) {
             return vehiculeMap.get(vehicule.getId());
         }
@@ -60,7 +74,7 @@ public class RecursiveConversionClientVehicule {
         
         // control de l'appel recursif de l'objet client
         if(vehicule.getClient() != null && !clientMap.containsKey(vehicule.getClient().getId())) {
-            vehiculeQueryDTO.setClient(convertClientToClientDTO(vehicule.getClient()));
+            vehiculeQueryDTO.setClient(convertClientToClientQueryDTO(vehicule.getClient()));
         }
         
         return vehiculeQueryDTO;
