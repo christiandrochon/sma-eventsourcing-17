230047b897ff9646d59a282f6a15e131d5455878
christiandrochon <khrisys@protonmail.com>
2024-08-29 23:48:10 +0200

    prise en compte de la CSP mais en la desactivant completement pour voir si ca vient bien d'elle dans les erreurs sma.local =ok

---
+++ src/main/java/fr/cdrochon/smamonolithe/configuration/CSPConfig.java
@@ -0,0 +1,69 @@
+package fr.cdrochon.smamonolithe.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CSPConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // desative la CSP
+                .headers(headers -> {}
+//                                 .defaultsDisabled()
+                        )
+//                .headers(headers -> headers
+//                                 .contentSecurityPolicy(csp -> csp
+//                                                                .policyDirectives("default-src 'self' http://sma.local; " +
+//                                                                                          "script-src 'self' 'unsafe-inline' http://sma.local; " +
+//                                                                                          "script-src-elem 'self' 'unsafe-inline' http://sma.local; " +
+//                                                                                          "script-src-attr 'self' 'unsafe-inline' http://sma.local; " +
+//                                                                                          "style-src 'self' 'unsafe-inline' http://sma.local; " +
+//                                                                                          "img-src 'self' data: http://sma.local; " +
+//                                                                                          "media-src 'self' data: http://sma.local; " +
+//                                                                                          "font-src 'self' http://sma.local; " +
+//                                                                                          "connect-src 'self' http://sma.local; " +
+//                                                                                          "frame-src 'self' http://sma.local; " +
+//                                                                                          "object-src 'self' http://sma.local; " +
+//                                                                                          "child-src 'self' http://sma.local; " +
+//                                                                                          "form-action 'self' http://sma.local;")
+//                                                       )
+//                        )
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                                               .anyRequest().permitAll()
+                                      )
+                .csrf(csrf -> csrf.disable())
+//                .formLogin(formLogin -> formLogin.disable())
+//                .httpBasic(httpBasic -> httpBasic.disable())
+        
+        //                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+        //                                               .anyRequest().authenticated()
+        //                                      )
+        //                .formLogin(formLogin -> formLogin
+        //                                   .permitAll()
+        //                          )
+        //                .logout(logout -> logout
+        //                        .permitAll()
+        //                       )
+        ;
+        return http.build();
+    }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://sma.local"); // Replace with your client's origin
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
