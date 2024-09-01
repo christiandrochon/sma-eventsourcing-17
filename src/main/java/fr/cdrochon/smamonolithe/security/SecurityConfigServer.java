f1df0960166c7e073fe216a34707bd29c9a28186
christiandrochon <khrisys@protonmail.com>
2024-09-01 21:21:22 +0200

    Les tests sur securityfilterchain sont en cours egalement ici, coté serveur. Je git aussi comme ca.

--- src/main/java/fr/cdrochon/smamonolithe/security/SecurityConfigServer.java
+++ src/main/java/fr/cdrochon/smamonolithe/security/SecurityConfigServer.java
@@ -6,20 +6,19 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfigServer {
-
+    
     private final JwtAuthConverter jwtAuthConverter;
-
+    
     public SecurityConfigServer(JwtAuthConverter jwtAuthConverter) {
         this.jwtAuthConverter = jwtAuthConverter;
     }
-
+    
     /**
      * Spring security supprime les frames ar defaut, car considere que c'est une faille de securité
      * <p>
@@ -39,61 +38,15 @@ public class SecurityConfigServer {
                 .csrf(Customizer.withDefaults())
                 //                .csrf().disable()
                 .cors(Customizer.withDefaults())
-                .headers(headers -> {})
+                .headers(headers -> {
+                })
                 .headers(h -> h.frameOptions(fo -> fo.disable()))
                 .csrf(crsf -> crsf.ignoringAntMatchers("/h2-console"))
-//                .authorizeRequests(ar -> ar.antMatchers("/v3/**", "/", "/dossiers").permitAll()
-               
-   
-//                .authorizeRequests(ar -> ar.antMatchers("/swagger-ui/**").permitAll())
-//
-//                // Configurer la gestion des sessions
-////                .sessionManagement()
-////                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-////                .and()
-//                .authorizeRequests()
-//                .antMatchers("/", "/index", "/templates/**", "/static/**", "/assets/**", "/garages").permitAll()
-
-
-                // Configurer les autorisations des requêtes
-                //                .authorizeRequests()
-                //                // Exempter les ressources statiques de la sécurité
-                //                .antMatchers(
-                //                        "/assets/**",
-                //                        "static/**",
-                //                        "templates/**",
-                //                        "/css/**",
-                //                        "/js/**",
-                //                        "/images/**",
-                //                        "/webjars/**",
-                //                        "/favicon.ico"
-                //                            ).permitAll()
-                //
-                //                // Autoriser certaines URL publiques
-                //                .antMatchers("/public/**").permitAll()
-
-                // Toute autre requête nécessite une authentification
-//                .anyRequest().authenticated())
-                
-
-//                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
-                // Exclure les ressources statiques
-                //                .authorizeRequests(ar -> ar.antMatchers("/assets/**", "/css/**", "/js/**", "/images/**").permitAll())
-                //                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
-                //                .authorizeHttpRequests(ar -> ar.requestMatchers("/h2-console/**").permitAll())
-                //                .authorizeHttpRequests(ar -> ar.requestMatchers("/v3/**").permitAll())
-                //                .authorizeHttpRequests(ar -> ar.requestMatchers("/swagger-ui/**").permitAll())
-                //                .authorizeHttpRequests(ar -> ar.requestMatchers("/command/client/**").permitAll())
-                //                .authorizeHttpRequests(ar->ar.requestMatchers("/garages").permitAll())
-//                .oauth2Login(Customizer.withDefaults())
-//                .logout(logout -> logout
-//                        .logoutSuccessUrl("/").permitAll()
-//                        .clearAuthentication(true)
-//                        .deleteCookies("JSESSIONID"))
-
-//                .oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2Login"))
-//                .oauth2Login(oauth2Login -> oauth2Login.defaultSuccessUrl("/"))
-//                .oauth2ResourceServer(o2 -> o2.jwt(token -> token.jwtAuthenticationConverter(jwtAuthConverter)))
+                .authorizeRequests(ar -> ar.antMatchers("/swagger-ui/**", "/v3/**").permitAll()
+                                           // Toute autre requête nécessite une authentification
+                                           .anyRequest().authenticated())
+                // verifie le token envoyé par le client thymeleaf grace le certificat public defini dans le fichier application.properties
+                .oauth2ResourceServer(o2 -> o2.jwt(token -> token.jwtAuthenticationConverter(jwtAuthConverter)))
                 .build();
     }
 }
