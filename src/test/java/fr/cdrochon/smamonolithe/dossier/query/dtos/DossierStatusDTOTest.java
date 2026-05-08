74174f6eda54a4198b414f41544f972aaef7cd50
christiandrochon <hello.christiandrochon.dev>
2026-05-08 15:41:07 +0200

    test(dossier-dto): ajout des tests unitaires qui couvrent les DTO, enum and base event

---
+++ src/test/java/fr/cdrochon/smamonolithe/dossier/query/dtos/DossierStatusDTOTest.java
@@ -0,0 +1,20 @@
+package fr.cdrochon.smamonolithe.dossier.query.dtos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DossierStatusDTOTest {
+
+    @Test
+    void shouldParseEnumIgnoringCase() {
+        assertEquals(DossierStatusDTO.ARCHIVE, DossierStatusDTO.forValue("archive"));
+        assertEquals(DossierStatusDTO.REOUVERT, DossierStatusDTO.forValue("ReOuVeRt"));
+    }
+
+    @Test
+    void shouldFallbackToOuvertWhenValueIsUnknown() {
+        assertEquals(DossierStatusDTO.OUVERT, DossierStatusDTO.forValue("inconnu"));
+    }
+}
+
