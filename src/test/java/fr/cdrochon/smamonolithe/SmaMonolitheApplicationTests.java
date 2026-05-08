b3b5ac4f223fd590f4e67548fd54d742d560c4e3
christiandrochon <hello.christiandrochon.dev>
2026-05-08 15:42:39 +0200

    test (config) : ajout du profil de test dans la main

--- src/test/java/fr/cdrochon/smamonolithe/SmaMonolitheApplicationTests.java
+++ src/test/java/fr/cdrochon/smamonolithe/SmaMonolitheApplicationTests.java
@@ -2,8 +2,10 @@ package fr.cdrochon.smamonolithe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmaMonolitheApplicationTests {
     
     @Test
